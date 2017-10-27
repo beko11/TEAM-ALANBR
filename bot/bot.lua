e7921e80bdb920f2c1ec7c7253ee9a25ca6c6282
beko11 <hasanfalah116622@gmail.com>
2017-10-27 10:33:45 +0300

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -88,14 +88,27 @@ function create_config( )
     "msg-checks",	
     "plugins",
     "tools",
-    "rank"
+    "rank",
+    "me",
+    "addkick",
+    "cleandel",
+    "cleanbot",
+    "cleanblock",
+    "del",
+    "deleall",
 	},
     sudo_users = {244252169},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
     info_text = [[
-	》• Ᏸ๑Ꮏ ＡＬＡＮＢᏒ 🤖 \n  ᱻᱻᱻᱻᱻᱻᱻᱻᱼᱼᱹᱹᱹ𖤍ᱹᱹᱹᱼᱼᱻᱻᱻᱻᱻᱻᱻᱻ \n  • ᗫЄᏉ Ᏸ๑Ꮏ ✔️ \n 👁‍🗨 • DeV  ┇ @D_1_T \n 👁‍🗨 • TwsL┇ @BEKO_TVBOT \n ᱻᱻᱻᱻᱻᱻᱻᱻᱼᱼᱹᱹᱹ𖤍ᱹᱹᱹᱼᱼᱻᱻᱻᱻᱻᱻᱻᱻ \n • ḈᏂ : @TEAM_ALANBR ۦ.🎶《
+	》• Ᏸ๑Ꮏ ＡＬＡＮＢᏒ 🤖 
+        ᱻᱻᱻᱻᱻᱻᱻᱻᱼᱼᱹᱹᱹ𖤍ᱹᱹᱹᱼᱼᱻᱻᱻᱻᱻᱻᱻᱻ 
+	• ᗫЄᏉ Ᏸ๑Ꮏ ✔️ 
+	👁‍🗨 • DeV  ┇ @D_1_T 
+	👁‍🗨 • TwsL┇ @BEKO_TVBOT 
+        ᱻᱻᱻᱻᱻᱻᱻᱻᱼᱼᱹᱹᱹ𖤍ᱹᱹᱹᱼᱼᱻᱻᱻᱻᱻᱻᱻᱻ 
+	• ḈᏂ : @TEAM_ALANBR ۦ.🎶《
 ]],
   }
 	serialize_to_file(config, './data/config.lua')

9e0436138f8e0999e8067ebd7a342adc370d57be
beko11 <hasanfalah116622@gmail.com>
2017-10-29 08:00:10 +0300

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
-if matches[1] == 'كول' then
+if matches[1] == 'كول' and is_mod(msg) then 
 local pext = matches[2]
 tdcli.sendMessage(msg.to.id, 0,1, pext,1,'html')
 end
